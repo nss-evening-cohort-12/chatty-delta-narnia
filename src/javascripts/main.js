2d4ddd7d421bf38168099cebeb8ec099937a98db
Ryan Beiden <rdbeiden@gmail.com>
2020-06-13 11:20:29 -0500

    wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,14 +1,37 @@
 import '../styles/main.scss';
 import utils from './helpers/utils';
+import messageData from './helpers/data/messageData';
 
-const deleteMessage = () => {
-  const domString = `
-    <div class="container">
-      <h2 style="font-size: 1.5em;">Ryan Beiden<h2>
-      <p style="font-size: .5em;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-    </div>
-  `;
-  utils.printToDom('#delete', domString);
+const wardrobes = messageData.getWardrobe();
+
+const printMessages = () => {
+  let domString = '';
+  wardrobes.forEach((wardrobe) => {
+    domString += `
+      <div class="container">
+        <hr>
+        <h2 style="font-size: 1.5em;">${wardrobe.userId}<h2>
+        <p style="font-size: .5em;">${wardrobe.messageContent}</p>
+        <a id="${wardrobe.messageId}"><i class="far fa-times-circle"></i></a>
+      </div>
+    `;
+    utils.printToDom('#delete', domString);
+  });
 };
 
-deleteMessage();
+const deleteMessage = (e) => {
+  e.preventDefault();
+  wardrobes.forEach((wardrobe) => {
+    if (wardrobe.messageId === e.target.id) {
+      console.error(`${wardrobe.messageId}`);
+      wardrobes.splice(wardrobe);
+    }
+  });
+  printMessages();
+};
+
+wardrobes.forEach((wardrobe) => {
+  $('body').on('click', `#${wardrobe.messageId}`, deleteMessage);
+});
+
+printMessages();
