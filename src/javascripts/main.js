1d8eced6c5bd38fa8c5485753bbbf0d7895d6972
Ryan Beiden <rdbeiden@gmail.com>
2020-06-13 10:15:11 -0500

    wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,1 +1,14 @@
 import '../styles/main.scss';
+import utils from './helpers/utils';
+
+const deleteMessage = () => {
+  const domString = `
+    <div class="container">
+      <h2 style="font-size: 1.5em;">Ryan Beiden<h2>
+      <p style="font-size: .5em;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+    </div>
+  `;
+  utils.printToDom('#delete', domString);
+};
+
+deleteMessage();
