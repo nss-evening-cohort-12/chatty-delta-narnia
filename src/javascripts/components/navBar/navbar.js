678d8623807c311286c527524b2fa4958a78b685
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-06-14 14:04:05 -0500

    wip

---
+++ src/javascripts/components/navBar/navbar.js
@@ -0,0 +1,56 @@
+import utils from '../../helpers/utils';
+import addMessages from '../../components/addMessages/addMessages'; //eslint-disable-line
+import styleChanges from '../../styleChanges';
+import 'bootstrap';
+import './navBar.scss';
+
+const buildNavbar = () => {
+  const domString = `
+      <div class="d-flex navbar">
+        <img style="width: 200px;" src="../../../src/assets/narnia-logo.png">
+        <div class="d-flex topnav" id="navbar">
+            <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              User
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+              <button class="dropdown-item" type="button" id="user1">Peter</button>
+              <button class="dropdown-item" type="button" id="user2">Susan</button>
+              <button class="dropdown-item" type="button" id="user3">Edmund</button>
+              <button class="dropdown-item" type="button" id="user4">Lucy</button>
+              <button class="dropdown-item" type="button" id="user5">Aslan</button>
+            </div>
+          </div>
+        <div class="form-inline">
+            <label class="sr-only" for="inlineFormInputName2">Message goes here</label>
+            <input type="text" class="form-control" id="messageBox" placeholder="Hello">
+            <button type="submit" class="btn btn-secondary" id="submit">Submit!</button>
+            <button type="button" class="btn btn-danger" id="clear">Clear Messages!</button>
+            <a id="dark-mode" class="toggle moon"><i class="fas fa-moon"></i></a>
+            <i type=button id=“largeText” class="fas fa-search-plus"></i>
+        </div>
+      </div>
+    `;
+  utils.printToDom('#theWitch', domString);
+  // document.querySelector('#dropdownMenu2').addEventListener('click', removeClasses); //eslint-disable-line
+  document.querySelector('#user1').addEventListener('click', getUserIdEvent); //eslint-disable-line
+  document.querySelector('#user2').addEventListener('click', getUserIdEvent); //eslint-disable-line
+  document.querySelector('#user3').addEventListener('click', getUserIdEvent); //eslint-disable-line
+  document.querySelector('#user4').addEventListener('click', getUserIdEvent); //eslint-disable-line
+  document.querySelector('#user5').addEventListener('click', getUserIdEvent); //eslint-disable-line
+  document.querySelector('#submit').addEventListener('click', addMessages.clickSubmitEvent);
+};
+
+const getUserIdEvent = (e) => {
+  const activeButtons = document.querySelectorAll('#user1, #user2, #user3, #user4, #user5');
+
+  for (let i = 0; i < activeButtons.length; i++) {
+    activeButtons[i].classList.remove('active');
+  }
+  e.target.classList.add('active');
+};
+
+$('body').on('click', '#dark-mode', styleChanges.darkMode);
+$('body').on('click', '#light-mode', styleChanges.lightMode);
+
+export default { buildNavbar, getUserIdEvent };
